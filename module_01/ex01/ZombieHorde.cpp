57dd88b90a65df508ea252d4fe9a19d52deb0b3f
Jules FEUILLATRE <jfeuilla@e2r9p4.clusters.42paris.fr>
2021-12-13 16:23:59 +0100

    ok

--- module_01/ex01/ZombieHorde.cpp
+++ module_01/ex01/ZombieHorde.cpp
@@ -17,6 +17,9 @@ Zombie* zombieHorde(int n, std::string name)
 	Zombie* horde = new Zombie[n];
 	
 	for(int i = 0; i < n; i++)
-		horde[i].set_name(name + std::to_string(i + 1));
+	{
+		name += 'a';
+		horde[i].set_name(name);
+	}
 	return horde;
 }

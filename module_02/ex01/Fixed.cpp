85f849333c4481c7c92006c7af9b2e73f465ff52
Jules FEUILLATRE <jfeuilla@e2r8p20.clusters.42paris.fr>
2021-12-14 17:32:15 +0100

    ok

--- module_02/ex01/Fixed.cpp
+++ module_02/ex01/Fixed.cpp
@@ -20,7 +20,7 @@ Fixed::Fixed( int const integer ) : _fractional_bits(8)
 Fixed::Fixed( float const floating ) : _fractional_bits(8)
 {
 	std::cout << "Float constructor called" << std::endl;
-	this->_fixed = std::roundf(floating * (1 << _fractional_bits));
+	this->_fixed = roundf(floating * (1 << _fractional_bits));
 }
 
 Fixed::~Fixed()

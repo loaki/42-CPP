28df1c189ae3043bbe507da128373a49d3996ab4
Jules FEUILLATRE <jfeuilla@e2r13p4.clusters.42paris.fr>
2022-03-14 17:15:24 +0100

    ok

--- module_05/ex03/Intern.hpp
+++ module_05/ex03/Intern.hpp
@@ -17,11 +17,6 @@ class Intern {
 		Intern& operator=(const Intern& other);
 
 		Form* makeForm(std::string name, std::string target);
-	private:
-		typedef struct s_forms {
-			std::string name;
-			Form *formClass;
-		} t_forms;
 };
 
 #endif
